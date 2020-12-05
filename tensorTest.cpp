018002e49cb25f7357d8e3b799d7ce23a3fd57f1
MMukundi <musicman5978@gmail.com>
2020-12-05 09:02:35 -0800

    Fixed test bug

--- tensorTest.cpp
+++ tensorTest.cpp
@@ -5,7 +5,7 @@ int main(){
     auto test = Tensor<double,3,1>({2,3,4});
     double& val = test[0];
     printf("Tensor val: %f %f %f\n",(double)test[0],(double)test[1],(double)test[2]);
-    test[3] = 1;
+    test[2] = 1;
     printf("Tensor val: %f %f %f\n",(double)test[0],(double)test[1],(double)test[2]);
     // printf("Tensor val: %f",(double)test[0]);
 }
