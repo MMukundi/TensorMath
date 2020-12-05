3bc01157018982d88d24be94f335077d03bf545c
MMukundi <musicman5978@gmail.com>
2020-12-05 06:52:35 -0800

    Added indexing declaration

--- Tensor.h
+++ Tensor.h
@@ -1,6 +1,15 @@
+template <typename Number>
+struct TensorData{
+    unsigned int m;
+    unsigned int n;
+    Number* tensor;
+public:
+    TensorData operator[](unsigned int);
+};
 template <typename Number, unsigned int m, unsigned int n>
 class Tensor{
 Number* data;
 public:
     Tensor();
+    TensorData<Number> operator[](unsigned int);
 };
